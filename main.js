32434159c5047c64bc1fc47e635cb3ae443a0a9b
Stekunda Henry <99985940+stekunda@users.noreply.github.com>
2023-02-06 16:06:05 -0500

    Homework 2 code (javascript)

--- main.js
+++ main.js
@@ -1,33 +1,20 @@
-class RunLengthEncode {
-    encode(givenString) {
-        let newString = "";
+class AccessLevel {
+    testAccess() {
+        // The numbers in the array below can be replaced with other arbitrary numbers if needed
+        let rights = [];
+        // The minPermission value can be replaced with an arbitrary number if needed
+        let minPermission = 18;
+        let result = "";
 
-        for (let i = 0; i < givenString.length; i++) {
-            let counter = 1;
-            while (
-                i < givenString.length - 1 &&
-                givenString[i] == givenString[i + 1]
-            ) {
-                counter++;
-                i++;
-            }
-
-            if (counter > 4) {
-                if (counter / 10 < 1) {
-                    newString =
-                        newString + "/" + "0" + counter + givenString[i];
-                } else {
-                    newString = newString + "/" + counter + givenString[i];
-                }
+        for (let i = 0; i < rights.length; i++) {
+            if (rights[i] >= minPermission) {
+                result += "A";
             } else {
-                for (let j = 0; j < counter; j++) {
-                    newString = newString + givenString[i];
-                }
+                result += "D";
             }
         }
-        return newString;
+        return result;
     }
 }
-
-let myString = new RunLengthEncode();
-console.log(myString.encode("aaaaahurrre"));
+let testRun = new AccessLevel();
+console.log(testRun.testAccess());
